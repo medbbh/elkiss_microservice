08060a810b8312e1f8bbd1e1f83c7088efe29863
medbbh <mbabah3450@gmail.com>
2025-04-02 09:56:02 +0000

    Update Allowed Hosts

--- elkiss_project/settings.py
+++ elkiss_project/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-lu(9n=ur@tii)940w2wxwpl*pb#+&#$upu-*0m1#^@6eev7eqi"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['elkiss.onrender.com']
 
 # Application definition
 
